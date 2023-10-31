a64544c972f6e8bfcd4941cb7c564092472cd1a5
21thkafka <caudeutsch@gmail.com>
2023-10-31 23:48:08 +0900

    jpa 인터페이스 테스트

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,5 +1,6 @@
 package study.datajpa.repository;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,5 +33,36 @@ class MemberRepositoryTest {
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);
 
+    }
+
+    @Test
+    public void basicCRUD(){
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        AssertionsForClassTypes.assertThat(findMember1).isEqualTo(member1);
+        AssertionsForClassTypes.assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.setUsername("member!!!!");
+
+        //목록 조회 검증
+        List<Member> all = memberRepository.findAll();
+        AssertionsForClassTypes.assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        AssertionsForClassTypes.assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deleteCount = memberRepository.count();
+        AssertionsForClassTypes.assertThat(deleteCount).isEqualTo(0);
     }
 }
