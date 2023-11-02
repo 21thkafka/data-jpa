85a8fdb3b7f6cd8bbf965bfc7c455ccec22f80fb
21thkafka <caudeutsch@gmail.com>
2023-11-02 23:21:57 +0900

    @Query 연습

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -15,4 +15,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
  //   @Query(name = "Member.findByUsername")    //이부분 주석처리 해도 도메인의 네임드쿼리를 먼저 찾아보고 실행해서 문제 발생 안함
     List<Member> findByUsername(@Param("username") String username);
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
